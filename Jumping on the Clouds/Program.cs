cef8be8427f846ff7741dc649880ddea7bbc0695
Rafy <Rafy@DESKTOP-ADC4ST3>
2019-03-12 14:28:16 +0200

    After i got accepted

--- Jumping on the Clouds/Program.cs
+++ Jumping on the Clouds/Program.cs
@@ -8,8 +8,40 @@ namespace Jumping_on_the_Clouds
 {
     class Program
     {
+        static int jumpingOnClouds(int[] c)
+        {
+            int jumpsCount = 0;
+            for (int i = 0; i < c.Length-1; i++)
+            {
+                if(i<c.Length-2&c[i + 1] == 0 && c[i + 2] == 0&& c[i] == 0)
+                {
+                    jumpsCount++;
+                    i++;
+                }
+                else if (c[i+1] == 0)
+                {
+                    jumpsCount++;
+                }
+               
+            }
+            return jumpsCount;
+
+        }
+
         static void Main(string[] args)
         {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] c = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp))
+            ;
+            int result = jumpingOnClouds(c);
+
+            Console.WriteLine(result);
+
+            //textWriter.Flush();
+           // textWriter.Close();
         }
     }
 }
